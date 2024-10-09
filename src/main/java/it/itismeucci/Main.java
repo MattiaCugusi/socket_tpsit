b4ee0f252ff008a54e7f2f84a33c349805a256ca
MattiaCugusi <m.cugusi287@gmail.com>
2024-10-09 09:36:37 +0200

    ricevimento lettera

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,6 +1,9 @@
 package it.itismeucci;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,9 +13,19 @@ public class Main {
         ServerSocket ss = new ServerSocket(5673);
         Socket mySocket = ss.accept();
         System.out.println("qualcuno si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+        DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
         
+        String stringaRicevuta = in.readLine();
+        System.out.println("la stringa ricevuta: " + stringaRicevuta);
 
+        String stringaMaiuscola = stringaRicevuta.toUpperCase();
+        out.writeBytes(stringaMaiuscola + "\n");
 
+        mySocket.close();
+
+        ss.close();
 
 
     
