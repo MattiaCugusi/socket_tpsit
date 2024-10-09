023c70ae2e53bff87542f4bc71fd486fabf132d4
MattiaCugusi <m.cugusi287@gmail.com>
2024-10-09 08:41:34 +0200

    server socket

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,7 +1,20 @@
 package it.itismeucci;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        ServerSocket ss = new ServerSocket(5673);
+        Socket mySocket = ss.accept();
+        System.out.println("qualcuno si è collegato");
+        
+
+
+
+
+    
     }
 }
